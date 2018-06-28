0e50a8b991776aa3029dbfcf3a1a958a9f0cb1fd
dyanakie <dyanakie@yahoo.com>
2018-06-28 15:19:55 +0300

    Added Reveal Functionality

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var chieftain = false;
 var boneCrusher = false;
 var hunter = false;
 
+var decisions = [6];
+
+for(var i = 0; i < decisions.length; i++){
+    decisions[i] = false;
+}
+
 $('.selectable').click(function(){
     $('#chosenOne').attr('src',$(this).attr('src'));
     $('#previewPic').attr('src',$(this).attr('src'));
@@ -12,13 +18,19 @@ $('.selectable').click(function(){
    // $('#description').html($(this).attr('alt'));
 });
 
-//commit test
+hide();
+
+function hide(){
+    $(".hidden").toggle();
+};
+
 
 // QUIZ 1
 
 $('#warrior').click(function(){
     chieftain, boneCrusher, hunter = false;
     warrior = true;
+    decisions[0] = true;
     document.querySelector("#class").innerHTML = "Warrior";
     document.querySelector("#answer1").innerHTML = "Answer: You are born in a family of Warriors.";
 
@@ -26,6 +38,7 @@ $('#warrior').click(function(){
 
 $('#boneCrusher').click(function(){
     chieftain, warrior, hunter = false;
+    decisions[0] = true;
     boneCrusher = true;
     document.querySelector("#class").innerHTML = "Bone Crusher";
     document.querySelector("#answer1").innerHTML = "Answer: You are born in a family of Bone Crushers.";
@@ -34,6 +47,7 @@ $('#boneCrusher').click(function(){
 
 $('#hunter').click(function(){
     chieftain, boneCrusher, warrior = false;
+    decisions[0] = true;
     hunter = true;
     document.querySelector("#class").innerHTML = "Hunter";
     document.querySelector("#answer1").innerHTML = "Answer: You are born in a family of Hunters.";
@@ -42,6 +56,7 @@ $('#hunter').click(function(){
 
 $('#chieftain').click(function(){
     warrior, boneCrusher, hunter = false;
+    decisions[0] = true;
     chieftain = true;
     document.querySelector("#class").innerHTML = "Chieftain";
     document.querySelector("#answer1").innerHTML = "Answer: You are born in a family of Chieftains.";
@@ -57,6 +72,7 @@ $('#truth').click(function(){
 
     truth = true;
     kill = false;
+    decisions[1] = true;
     document.querySelector("#class").innerHTML = "Chieftain";
     document.querySelector("#answer2").innerHTML = "Answer: You tell the Truth and Balgrub returns as the hero.";
 
@@ -66,6 +82,7 @@ $('#kill').click(function(){
 
     truth = true;
     kill = false;
+    decisions[1] = true;
     document.querySelector("#class").innerHTML = "Chieftain";
     document.querySelector("#answer2").innerHTML = "Answer: You quickly finish him and return as a hero.";
 
@@ -80,6 +97,7 @@ $('#jump').click(function(){
 
     jump = true;
     wait = false;
+    decisions[2] = true;
     
     document.querySelector("#answer3").innerHTML = "Answer: You jump blood crazed towards the enemy.As the Orcish Heroes of old, you charge with berserker fury!";
 
@@ -89,6 +107,7 @@ $('#wait').click(function(){
 
     wait = true;
     jump = false;
+    decisions[2] = true;
     
     document.querySelector("#answer3").innerHTML = "Answer: You are brave but not stupid.You hold the position and wait for the others to reach you.";
 
@@ -107,6 +126,7 @@ $('#prisonersKill').click(function(){
     prisonersEnslave = false;
     prisonersMines = false;
     prisonersFree = false;
+    decisions[3] = true;
     
     document.querySelector("#answer4").innerHTML = "Answer: Filthy humans.You slaughter all of them without a drop of mercy.";
 
@@ -118,6 +138,7 @@ $('#prisonersEnslave').click(function(){
     prisonersEnslave = true;
     prisonersMines = false;
     prisonersFree = false;
+    decisions[3] = true;
     
     document.querySelector("#answer4").innerHTML = "Answer: You slaughter the suriving men and take the rest to serve their new masters.";
 
@@ -129,6 +150,7 @@ $('#prisonersFree').click(function(){
     prisonersEnslave = false;
     prisonersMines = false;
     prisonersFree = true;
+    decisions[3] = true;
     
     document.querySelector("#answer4").innerHTML = "Answer: In an act of great mercy you let them go.They cant believe and thank you with tears in their eyes.";
 
@@ -140,6 +162,7 @@ $('#prisonersMines').click(function(){
     prisonersEnslave = false;
     prisonersMines = true;
     prisonersFree = false;
+    decisions[3] = true;
     
     document.querySelector("#answer4").innerHTML = "Answer: You gather them and send them to work for the Orcish economy.Most will die within one year but who cares.";
 
@@ -159,6 +182,7 @@ $('#prisonersMines').click(function(){
        balgrubKill = false;
        balgrubImprison = false;
        balgrubStepDown = false;
+       decisions[4] = true;
     
     document.querySelector("#answer5").innerHTML = "Answer: Only the strong can rule.Everybody makes a circle and you step towards your old friend, Great Axe in hand.";
 
@@ -170,6 +194,7 @@ $('#balgrubKill').click(function(){
     balgrubKill = true;
     balgrubImprison = false;
     balgrubStepDown = false;
+    decisions[4] = true;
  
  document.querySelector("#answer5").innerHTML = "Answer: Chieftains don't have to do everything themselves.You give the order and your men jump on Balgrub from all sides.";
 
@@ -181,6 +206,7 @@ $('#balgrubImprison').click(function(){
     balgrubKill = false;
     balgrubImprison = true;
     balgrubStepDown = false;
+    decisions[4] = true;
  
  document.querySelector("#answer5").innerHTML = "Answer: He is your friend after all.You give the order and your men disarm him and take him prisoner.Not before he kills 5 of them.";
 
@@ -192,6 +218,7 @@ $('#balgrubStepDown').click(function(){
     balgrubKill = false;
     balgrubImprison = false;
     balgrubStepDown = true;
+    decisions[4] = true;
  
  document.querySelector("#answer5").innerHTML = "Answer: He is the stronger and you can only admit that.Commander is not so bad after all.You step down peacefully.";
 
@@ -204,18 +231,90 @@ $('#balgrubStepDown').click(function(){
    var finalAmbush = false;
    var finalRetreat = false;
 
+   $('#finalAttack').click(function(){
 
+    finalAttack = true;
+    finalRetreat = false;
+    finalSuePeace = false;
+    finalAmbush = false;
+    decisions[5] = true;
+ 
+   document.querySelector("#answer6").innerHTML = "Answer: You decide to do this the Orc way.Frontal Assault with all your might.The two armies face each other and take battle order.Your Bone Crushers scream with battlelust, morale cannot be higher.Forward for GLORY AND DEATH!";
+
+   });
+
+   $('#finalSuePeace').click(function(){
+
+    finalAttack = false;
+    finalRetreat = false;
+    finalSuePeace = true;
+    finalAmbush = false;
+    decisions[5] = true;
+ 
+   document.querySelector("#answer6").innerHTML = "Answer: This land and our people have seen too much war.You wait for their army to enter the field and then send peace envoys.You hope for the best.";
+
+   });
+
+   $('#finalRetreat').click(function(){
+
+    finalAttack = false;
+    finalRetreat = true;
+    finalSuePeace = false;
+    finalAmbush = false;
+    decisions[5] = true;
+ 
+   document.querySelector("#answer6").innerHTML = "Answer: They are too many.You cannot risk so many orc's lives.You scorch the camp and retreat to the mountains.";
+
+   });
+
+
+   $('#finalAmbush').click(function(){
+
+    finalAttack = false;
+    finalRetreat = false;
+    finalSuePeace = false;
+    finalAmbush = true;
+    decisions[5] = true;
+ 
+   document.querySelector("#answer6").innerHTML = "Answer: A wise commander uses every advantage.You give the command and your orc army lies in wait around the mountain passes.You are going to give them one nasty surprise.";
+
+   });
 
 
 // Reveal
 
 document.querySelector("#terrifying").innerHTML = "Terrifying: "+terrifying;
 
+var unveiled = false;
+
 $('#reveal').click(function(){
 
-    document.querySelector("#class").innerHTML = "Chieftain";
-    document.querySelector("#answer3").innerHTML = "Answer: You quickly finish him and return as a hero.";
+    var doneAll = true;
+
+    for(var i = 0; i<decisions.length; i++){
+        if(!decisions[i]){
+            doneAll = false;
+            break;
+        }
+    }
+
+    var doneAll = true;
+
+    if(!unveiled && doneAll){
+         scoobyDo();
+         $(".hidden").toggle();
+         unveiled = true;
+         $("#quiz").toggle();
+         $("#picture").toggle();
+    }
 
 });
+
+function scoobyDo(){
+
+var strenght = 0;
+var prestige = 0;
+var terrible = 0;
+}
 
 
